de66c96448d2c4a07ffcebcf37073e9da4ddfc13
NDTue <ductue2003@gmail.com>
2025-05-03 10:26:20 +0700

    [WeatherInfo] Fomart Time

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -14,23 +14,42 @@ const WeatherInfo = ({ weatherData }) => {
 
     // Hàm convert Country Code -> Country Name
     const getCountryName = (countryCode) => {
-        const regionNames = new Intl.DisplayNames(["eng"], { type: "region" }); // Hiển thị tên quốc gia bằng tiếng Anh
+        const regionNames = new Intl.DisplayNames(["en"], { type: "region" }); // Hiển thị tên quốc gia bằng tiếng Anh
         return regionNames.of(countryCode) || countryCode; // Nếu không tìm thấy, trả về mã gốc
     }
 
+    // Hàm chuyển timestamp → ngày giờ theo GMT+7
+    const formatGMT7 = (timestamp) => {
+        const date = new Date(timestamp * 1000); // Convert timestamp (giây) thành đối tượng Date (miligiây)
+        return date.toLocaleDateString("en-GB", { 
+            timeZone: "Asia/Ho_Chi_Minh", // Cố định múi giờ Việt Nam (GMT+7)
+            day: "numeric",               // Hiển thị ngày dưới dạng số 
+            month: "long",                // Hiển thị tháng dạng chữ đầy đủ (April, May,...)
+            year: "numeric",              // Hiển thị năm đầy đủ (2025)
+        });
+    };
+
     return (
         <div className=''>
+            <h1>Today</h1>
+
+            {/* Location */}
             <h2>
                 {weatherData.name}, {getCountryName(weatherData.sys.country)}
             </h2>
 
-            <p>Temperature: {weatherData.main.temp}℃ </p>
+            <div className=''>
+                <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+                    alt="Weather Icon"
+                />
+                <p>Temperature: {weatherData.main.temp}℃ </p>
+            </div>
+            
             <p className='capitalize'>Weather: {weatherData.weather[0].description} </p>
-            <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
-                alt="Weather Icon"
-            />
+            <p>Today: {formatGMT7(weatherData.dt)}</p>
+            
         </div>
-    );
+    )
 };
 
 export default WeatherInfo;
