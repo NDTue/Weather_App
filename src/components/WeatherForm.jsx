f92b64e64ce6d3e41d476e382cd5f3ffd70a88c7
NDTue <ductue2003@gmail.com>
2025-05-03 10:25:33 +0700

    [WeatherForm] Refactor

--- src/components/WeatherForm.jsx
+++ src/components/WeatherForm.jsx
@@ -15,21 +15,23 @@ const WeatherForm = ({ onSearch }) => {
 
     return (
         <>
-            <form onSubmit={handleSubmit} className='flex justify-center items-center mt-5'>
-                <input className='border-2 rounded-lg  w-full mr-4 p-3 focus:outline-teal-400'
-                    type="text" placeholder='Enter city name'
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)} // Cập nhật tên thành phố 
-                />
-
-                <button class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium 
-                    rounded-lg border border-transparent bg-teal-200 
-                    text-teal-700 hover:scale-105 duration-300 focus:outline-hidden"
-                     type="submit"
-                >
-                    Button
-                </button>
-            </form>
+            <div className='max-w-96 md:max-w-[78rem] my-0 mx-auto'>
+                <form onSubmit={handleSubmit} className='flex justify-center items-center mt-5 w-full'>
+                    <input className='border-2 rounded-xl w-72 md:w-full md:mr-4 p-3 focus:outline-teal-400'
+                        type="text" placeholder='Enter city name'
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)} // Cập nhật tên thành phố 
+                    />
+    
+                    <button className="hidden w-1/4 md:block py-3 px-4 flex items-center gap-x-2 text-sm font-medium 
+                        rounded-lg border border-transparent bg-teal-200 
+                        text-teal-700 hover:scale-105 duration-300 focus:outline-hidden"
+                         type="submit"
+                    >
+                        Button
+                    </button>
+                </form>
+            </div>
         </>
     );
 };
